55671616a927f2bb12fb863daf4d45a954da5532
Likhon1221 <siddiqur.rahman1221Ggmail.com>
2024-08-31 22:44:21 -0400

    Task 2 - Product Inventory Management

--- cc_1.js
+++ cc_1.js
@@ -6,7 +6,7 @@ var isActive = true;
 
 console.log(employeeName, typeof(employeeName));
 console.log(employeeID, typeof(employeeID));
-console. log(isActive, typeof(isActive));
+console.log(isActive, typeof(isActive));
 
 
 
