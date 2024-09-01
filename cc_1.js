efc5626f1198785a99e4031f65bb521f8f80eb4a
Likhon1221 <siddiqur.rahman1221Ggmail.com>
2024-08-31 22:01:43 -0400

    Task 2 - Product Inventory Management

--- cc_1.js
+++ cc_1.js
@@ -7,3 +7,16 @@ var isActive = true;
 console.log(employeeName, typeof(employeeName));
 console.log(employeeID, typeof(employeeID));
 console. log(isActive, typeof(isActive));
+
+
+
+//Task 2: Compound Data Types
+
+let products = ["Desktop", "Phone", "Laptop"];
+const productDetails ={
+         name: products,
+         price: 1000.00,
+         inStock: 10
+}
+
+console. log(products, productDetails);
